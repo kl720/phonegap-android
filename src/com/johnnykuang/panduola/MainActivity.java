6108d8f5c466ee941d6aa40dacd3bbaa39cc595d
kl720 <kl_apple@126.com>
2014-11-17 20:14:07 +0800

    jjj

---
+++ src/com/johnnykuang/panduola/MainActivity.java
@@ -0,0 +1,20 @@
+package com.johnnykuang.panduola;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+
+public class MainActivity extends Activity{
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+	}
+	
+	public void registerKey()
+	{
+		
+	}
+}
